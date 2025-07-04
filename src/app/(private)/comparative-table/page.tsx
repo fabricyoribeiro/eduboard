424e893b9353121587164a517a2d59c66a65b918
Fabricyo <fabricyosilva600@gmail.com>
2025-07-04 10:54:25 -0300

    performance predict working

--- src/app/(private)/comparative-table/page.tsx
+++ src/app/(private)/comparative-table/page.tsx
@@ -1,9 +1,45 @@
 import Image from "next/image";
+import { BASE_URL } from "../../../constants/baseUrl";
 
-export default function ComparativeTable() {
+export default async function ComparativeTable() {
+
+  const response = await fetch(`${BASE_URL}/analytics/performance-classification`)
+  const data = await response.json()
+
+  console.log(data)
+
   return (
-    <div className="bg-blue-50 w-full h-full">
-      Tabela comparativa
+    <div className="px-8">
+
+      <table className="table mt-8 ">
+        <thead>
+          <tr>
+            <th>Aluno</th>
+            <th>Acertos (%)</th>
+            <th>Tempo de jogo (segundos)</th>
+            <th>Tempo médio por tentativa (segundos)</th>
+            <th>Tentativas</th>
+            <th>Desempenho</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data?.map((item) => (
+              <tr >
+                <td>{item.actor}</td>
+                <td>{item.accuracy}</td>
+                <td>{item.game_time}</td>
+                <td>{item.average_time_per_attempt}</td>
+                <td>{item.attempts}</td>
+                <td>{item.performance}</td>
+
+              </tr>
+
+            ))
+          }
+        </tbody>
+      </table>
     </div>
+
   );
 }
--- src/app/(private)/individual-analysis/page.tsx
+++ src/app/(private)/individual-analysis/page.tsx
@@ -7,7 +7,6 @@ export default async function Page() {
   const response = await fetch(`${BASE_URL}/actor/all`)
   const users = await response.json()
 
-  console.log('users', users)
 
 
   return <IndividualAnalysis users={users} />;
