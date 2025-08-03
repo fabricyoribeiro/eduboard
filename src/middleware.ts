0dae6eefed1096382dae7a5b336bcd3f622bc522
Fabricyo <fabricyosilva600@gmail.com>
2025-08-03 15:01:45 -0300

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 import { MiddlewareConfig, NextRequest, NextResponse } from "next/server"
 
 const publicRoutes = [
-  {path: '/login', whenAuthenticated: 'redirect'}, {path: '/register', whenAuthenticated: 'redirect'}
+  { path: '/login', whenAuthenticated: 'redirect' }, { path: '/register', whenAuthenticated: 'redirect' }
 ] as const
 
 const REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE = '/login'
 
-export function middleware(request: NextRequest){
+export function middleware(request: NextRequest) {
   //temporario apenas para nao funcionar o redirect
   // return NextResponse.next()
 
@@ -15,11 +15,11 @@ export function middleware(request: NextRequest){
   const publicRoute = publicRoutes.find(route => route.path === path)
   const authToken = request.cookies.get('eduboard_token')
 
-  if (!authToken && publicRoute){
+  if (!authToken && publicRoute) {
     return NextResponse.next()
   }
 
-  if(!authToken && !publicRoute){
+  if (!authToken && !publicRoute) {
     const redirectUrl = request.nextUrl.clone()
     redirectUrl.pathname = REDIRECT_WHEN_NOT_AUTHENTICATED_ROUTE
     return NextResponse.redirect(redirectUrl)
@@ -27,15 +27,15 @@ export function middleware(request: NextRequest){
 
   if (authToken && publicRoute?.whenAuthenticated === 'redirect') {
     const redirectUrl = request.nextUrl.clone()
-    return NextResponse.redirect('/')
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
-  if(authToken &&!publicRoute){
+  if (authToken && !publicRoute) {
     //verificar se o JWT esta expirado
     //se sim, remover o cookie e redirecionar para login
     return NextResponse.next()
   }
-  
+
   return NextResponse.next()
 }
 
